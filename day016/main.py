73727866c758b0b0704bd7ac0b138610ba3b8b43
Eduardo R. Abe <eduardorabe@gmail.com>
2021-06-12 09:04:31 -0300

    Day 16 done!

---
+++ day016/main.py
@@ -0,0 +1,27 @@
+# 100 Days of Code
+# DAY 016
+# Created by eduardorabe
+# Coffee Machine in OOP
+
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
